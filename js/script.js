e80520e85ae714a0560c70a8eff6897e3e60f063
iuliana-antochi <icantochi@gmail.com>
2022-08-27 01:28:48 +0300

    updating avatar photo

--- js/script.js
+++ js/script.js
@@ -4,18 +4,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   heart.style.color = "#" + randomColor;
 });
-
-const imgs = [
-  "../imgs/cat-programming-1.png",
-  "../imgs/cat-programming-2.png",
-  "../imgs/cat-programming-3.png",
-  "../imgs/cat-programming-4.png",
-  "../imgs/cat-programming-5.png",
-  "../imgs/cat-programming-6.png"
-];
-const size = imgs.length;
-const x = Math.floor(size*Math.random())
-console.log(x)
-console.log(imgs[x])
-document.getElementById('changing-photo').src = imgs[x];
-
